224f55efa3e0bc8bf8e5b23326aad2d81c28076e
Aleksei Usov <54838435+AlekseiUsov@users.noreply.github.com>
2023-11-09 08:59:40 +0300

    add checkbox and closeicon in todo

--- src/ui/dayTodos/listTodos/todo/todo.tsx
+++ src/ui/dayTodos/listTodos/todo/todo.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 
 // styles
 import styles from "./todo.module.scss";
@@ -6,14 +6,18 @@ import styles from "./todo.module.scss";
 // types
 import { ITodo } from "../../../../types/todos";
 
+// icons
+import { CloseIcon } from "../../../../assets/icons";
+
 export const ToDo: FC<ITodo> = ({ isDone, todo }) => {
   return (
     <li className={styles.todo}>
-      <div>
-        <div>
-          <span className={isDone ? styles.done : ""}> {todo}</span>
-        </div>
-      </div>
+      <label>
+        <input type="checkbox" className={styles.todo__real_checkbox} />
+        <span className={styles.todo__custom_checkbox} />
+        <span className={styles.text}>{todo}</span>
+      </label>
+      <span className={styles.todo__close}>{CloseIcon}</span>
     </li>
   );
 };
